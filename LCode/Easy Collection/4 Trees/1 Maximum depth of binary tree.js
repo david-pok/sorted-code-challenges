a1bd567c2ef6e6f407d2466d7a9c2b428afb028c
David <david.pok@gmail.com>
2021-05-28 16:54:37 -0700

    adds 1 answer to depth of binary tree

--- LCode/Easy Collection/4 Trees/1 Maximum depth of binary tree.js
+++ LCode/Easy Collection/4 Trees/1 Maximum depth of binary tree.js
@@ -1,7 +1,7 @@
 // Given the root of a binary tree, return its maximum depth.
 
-// A binary tree's maximum depth is the number of nodes 
-// along the longest path from the root node down to the 
+// A binary tree's maximum depth is the number of nodes
+// along the longest path from the root node down to the
 // farthest leaf node.
 
 // Example 1:
@@ -9,7 +9,7 @@
 //         /  \
 //        9    20
 //            /  \
-//           15   7 
+//           15   7
 
 // Input: root = [3,9,20,null,null,15,7]
 // Output: 3
@@ -28,10 +28,40 @@
 
 // Input: root = [0]
 // Output: 1
- 
 
 // Constraints:
 
 // The number of nodes in the tree is in the range [0, 104].
 // -100 <= Node.val <= 100
 
+const maxDepth = function (root) {
+  //bfs approach
+  //make a queue to put our current node and its children into
+  //remove the parent node and add in the childrens childrens
+  //keep a running count of depth, incrementing per removal
+  //keep going until our stack goes empty
+  if (!root) {
+    return 0;
+  }
+
+  let q = [];
+  let depth = 0;
+
+  q.push(root);
+
+  while (q.length > 0) {
+    depth += 1;
+    let temp = [];
+
+    for (let i = 0; i < q.length; i++) {
+      if (q[i].left) {
+        temp.push(q[i].left);
+      }
+      if (q[i].right) {
+        temp.push(q[i].right);
+      }
+    }
+    q = temp;
+  }
+  return depth;
+};
