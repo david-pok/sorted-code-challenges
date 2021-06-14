5c2555c614e8ed3ea1bba6e617f20775553a647b
David <david.pok@gmail.com>
2021-06-14 14:39:04 -0700

    completes climbing stairs

--- LCode/Easy Collection/6 Dynamic Programming/1 Climbing Stairs.js
+++ LCode/Easy Collection/6 Dynamic Programming/1 Climbing Stairs.js
@@ -1,9 +1,7 @@
 // You are climbing a staircase. It takes n steps to reach the top.
 
-// Each time you can either climb 1 or 2 steps. 
+// Each time you can either climb 1 or 2 steps.
 // In how many distinct ways can you climb to the top?
-
- 
 
 // Example 1:
 
@@ -21,13 +19,17 @@
 // 1. 1 step + 1 step + 1 step
 // 2. 1 step + 2 steps
 // 3. 2 steps + 1 step
- 
 
 // Constraints:
 
 // 1 <= n <= 45
 
-const climbStairs = function(n) {
-    //perhaps recursively hit each  amount of steps?
-    //maybe keep track of each one with an object...
+let memo = {};
+const climbStairs = function (n) {
+  if (n < 3) return n;
+
+  if (!(n in memo)) {
+    memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
+  }
+  return memo[n];
 };
