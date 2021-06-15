1fe5abe563bb7f5a114161419237f02202f0fc68
David <david.pok@gmail.com>
2021-06-15 16:45:34 -0700

    adds max subarray

--- LCode/Easy Collection/6 Dynamic Programming/2 Best Time to Buy and Sell Stocks.js
+++ LCode/Easy Collection/6 Dynamic Programming/2 Best Time to Buy and Sell Stocks.js
@@ -1,33 +1,58 @@
-// You are given an array prices where prices[i] 
+// You are given an array prices where prices[i]
 // is the price of a given stock on the ith day.
 
-// You want to maximize your profit by choosing 
-// a single day to buy one stock and choosing a 
+// You want to maximize your profit by choosing
+// a single day to buy one stock and choosing a
 // different day in the future to sell that stock.
 
-// Return the maximum profit you can achieve from 
+// Return the maximum profit you can achieve from
 // this transaction. If you cannot achieve any profit, return 0.
-
- 
 
 // Example 1:
 
 // Input: prices = [7,1,5,3,6,4]
 // Output: 5
-// Explanation: Buy on day 2 (price = 1) and 
+// Explanation: Buy on day 2 (price = 1) and
 // sell on day 5 (price = 6), profit = 6-1 = 5.
-// Note that buying on day 2 and selling on day 
+// Note that buying on day 2 and selling on day
 // 1 is not allowed because you must buy before you sell.
 
 // Example 2:
 
 // Input: prices = [7,6,4,3,1]
 // Output: 0
-// Explanation: In this case, no transactions are done 
+// Explanation: In this case, no transactions are done
 // and the max profit = 0.
- 
 
 // Constraints:
 
 // 1 <= prices.length <= 105
 // 0 <= prices[i] <= 104
+
+//brute force/bad answer
+var maxProfit = function (prices) {
+  let profit = 0;
+  let min = prices[0];
+
+  for (let i = 1; i < prices.length; ++i) {
+    if (min > prices[i]) {
+      min = prices[i];
+    } else if (prices[i] - min > profit) {
+      profit = prices[i] - min;
+    }
+  }
+
+  return profit;
+};
+
+//good answer
+const maxProfit = function (prices) {
+  let min = prices[0];
+  let prof = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < min) min = prices[i];
+    if (prices[i] - min > prof) prof = prices[i] - min;
+  }
+  return prof;
+};
