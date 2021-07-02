2ae523d0850b0ba6814f036f8369052860c78fb1
David <david.pok@gmail.com>
2021-07-02 16:40:31 -0700

    adds chunk reverse

---
+++ Other/Reverse in Chunks.js
@@ -0,0 +1,18 @@
+// given a string input , reverse the ints in chunks of k 
+// where k is the int that comes after the ;
+
+// so for the input 1,2,3,4,5;2
+// k = 2
+// output = 2,1,4,3,5
+
+
+
+
+// for input 1,2,3,4,5,6,7,8,9;3
+// k = 3
+// output = 3,2,1,6,5,4,9,8,7
+
+
+const chunkReverse = input => {
+
+}
