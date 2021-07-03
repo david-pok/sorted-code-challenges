12cf3da721a22b4505ae28e65747a9cf83339cf9
David <david.pok@gmail.com>
2021-07-02 17:03:52 -0700

    adds explanation

--- Other/Reverse in Chunks.js
+++ Other/Reverse in Chunks.js
@@ -10,6 +10,14 @@
 // output = 3,2,1,6,5,4,9,8,7
 
 const chunkReverse = (inp) => {
+  //first take str input and split it so that we can separate k
+  //and also have our input in an array of ints
+  //then we loop through array of ints incrementing by k
+  //we find the lowest index of the chunk
+  //the highest index of the chunk
+  //and swap them while working our way towards the middle of the chunk
+  //swapping each elem we go by
+  //at the end we return our array as a str
   const splitted = inp.split(";");
   const digits = splitted[0].split(",").map(Number);
   const k = splitted.pop();
@@ -17,7 +25,7 @@ const chunkReverse = (inp) => {
   for (let i = 0; i < digits.length; i += k) {
     if (i + k >= digits.length) break;
     let low = i,
-      high = Math.min(i + k - 1, digits.length - 1);
+      high = i + k - 1;
 
     while (low < high) {
       [digits[low], digits[high]] = [digits[high], digits[low]];
@@ -27,3 +35,6 @@ const chunkReverse = (inp) => {
   }
   return digits.join(",");
 };
+
+console.log(chunkReverse("1,2,3,4,5;2"));
+console.log(chunkReverse("1,2,3,4,5,6,7,8,9;3"));
