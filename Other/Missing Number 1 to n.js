6239be5d24c16f765b5719f9c1efbfc7ab0f264f
David <david.pok@gmail.com>
2021-07-04 16:25:24 -0700

    adds solutions

--- Other/Missing Number 1 to n.js
+++ Other/Missing Number 1 to n.js
@@ -25,11 +25,33 @@ const oneMissing = (numbers) => {
   return realSum - numbSum;
 };
 
+const oneMissing2 = (numbers) => {
+    for (let i = 0; i < numbers.length; i++) {
+        if (i + 1 !== numbers[i]) {
+            return i + 1;
+        }
+    }
+}
+
+const testArr = [1,2,3,4,5,6,8];
 //this is to create an array 1 to 100 with a missing number
 const arr = [...Array(100+1).keys()].slice(1);
 const rand = Math.ceil(Math.random(100) * 100);
 arr.splice(rand, 1);
 
-console.log(oneMissing(arr));
+console.log(oneMissing2(testArr));
 
 //for 2 missing
+
+// We multiply all the numbers in the array we’re given 
+// and find the total product.
+
+// Then, we multiply all the numbers from 1 to N and 
+// find that total product.
+
+// Then, we divide the second product by the first product 
+// and that will give us n1 * n2.
+
+// Now, we use some algebra (with n1 * n2 and n1 + n2) 
+// to create a quadratic equation that will give us the 
+// value of n1 and n2.
