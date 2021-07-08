10667ccd3e6492159acf2d1ba7855f3eb6baf6c9
David <david.pok@gmail.com>
2021-07-07 22:53:03 -0700

    adds another solution

--- LCode/other/20 Valid Parentheses.js
+++ LCode/other/20 Valid Parentheses.js
@@ -35,26 +35,42 @@
 
 //my solution
 var isValid = function (s) {
-    if (s.length == 0 || s.length == 1 || s.length % 2 !== 0) return false;
-    const stack = [];
-    for (let char in s) {
-      if (s[char] == "(") {
-        stack.push(")");
-        continue;
-      }
-      if (s[char] == "{") {
-        stack.push("}");
-        continue;
-      }
-      if (s[char] == "[") {
-        stack.push("]");
-        continue;
-      }
-  
-      if (s[char] == ")" || s[char] == "}" || s[char] == "]") {
-        const check = stack.pop();
-        if (s[char] !== check) return false;
-      }
+  if (s.length == 0 || s.length == 1 || s.length % 2 !== 0) return false;
+  const stack = [];
+  for (let char in s) {
+    if (s[char] == "(") {
+      stack.push(")");
+      continue;
     }
-    return !stack.length;
+    if (s[char] == "{") {
+      stack.push("}");
+      continue;
+    }
+    if (s[char] == "[") {
+      stack.push("]");
+      continue;
+    }
+
+    if (s[char] == ")" || s[char] == "}" || s[char] == "]") {
+      const check = stack.pop();
+      if (s[char] !== check) return false;
+    }
+  }
+  return !stack.length;
+};
+
+//another solution using hash map
+var isValid = function (s) {
+  const stack = [];
+  const complement = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
   };
+
+  for (let char of s) {
+    if (!complement[char]) stack.push(char);
+    else if (stack.pop() !== complement[char]) return false;
+  }
+  return stack.length === 0;
+};
