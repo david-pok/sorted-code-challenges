97fa7112405932a8903e3339a148730a2b10dbf2
David <david.pok@gmail.com>
2021-07-08 17:56:06 -0700

    adds a much better solution

--- LCode/other/13 Roman to Integer.js
+++ LCode/other/13 Roman to Integer.js
@@ -46,6 +46,7 @@
 // s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 // It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 
+//my solution
 const romanToInt = function (s) {
   const map = {
     I: 1,
@@ -81,4 +82,24 @@ const romanToInt = function (s) {
     }
   }
   return total;
+};
+
+//MUCH BETTER ANSWERS
+var romanToInt = function (s) {
+  symbols = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  value = 0;
+  for (let i = 0; i < s.length; i += 1) {
+    symbols[s[i]] < symbols[s[i + 1]]
+      ? (value -= symbols[s[i]])
+      : (value += symbols[s[i]]);
+  }
+  return value;
 };
