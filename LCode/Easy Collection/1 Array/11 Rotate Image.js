06416ec32eed8e41b91cb9b9525b20ef362e87ad
David <david.pok@gmail.com>
2021-07-10 17:21:26 -0700

    adds fuzzy find

--- LCode/Easy Collection/1 Array/11 Rotate Image.js
+++ LCode/Easy Collection/1 Array/11 Rotate Image.js
@@ -2,8 +2,6 @@
 
 // You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
 
- 
-
 // Example 1:
 // Input: matrix = [
 //     [1,2,3],
@@ -54,38 +52,36 @@
 // 1 <= n <= 20
 // -1000 <= matrix[i][j] <= 1000
 
-const rotate = function(matrix) {
-    //first way to do this
-    //start by rotating all the corner elements
-    //and then we move on the rotate the elements clockwise of corners
-    //keep iterating until we reach our end
+const rotate = function (matrix) {
+  //first way to do this
+  //start by rotating all the corner elements
+  //and then we move on the rotate the elements clockwise of corners
+  //keep iterating until we reach our end
   let left = 0;
   let right = matrix.length - 1;
 
-  while(left < right) {
-    for(let i = 0; i < right - left; i++) {
+  while (left < right) {
+    for (let i = 0; i < right - left; i++) {
       let top = left;
       let bottom = right;
-      let tempTopLeft = matrix[top][left + i]
+      let tempTopLeft = matrix[top][left + i];
       matrix[top][left + i] = matrix[bottom - i][left];
       matrix[bottom - i][left] = matrix[bottom][right - i];
-      matrix[bottom][right - i] = matrix[top + i][right]
+      matrix[bottom][right - i] = matrix[top + i][right];
       matrix[top + i][right] = tempTopLeft;
-
     }
-    right-=1;
-    left+=1;
+    right -= 1;
+    left += 1;
   }
-    
-    
-    //second way of doing this
-    //transpose the elements
-    //transpose means to swaps all the diagonals
-    //which would turn all our rows into columns
-    //and all our columns into rows
-    //once we transpose, we can just reverse all the arrays to get our target
+
+  //second way of doing this
+  //transpose the elements
+  //transpose means to swaps all the diagonals
+  //which would turn all our rows into columns
+  //and all our columns into rows
+  //once we transpose, we can just reverse all the arrays to get our target
   // for (let i = 0; i < matrix.length; i++) {
-  // for(let j = 0; j < i; j++) {
+  //   for (let j = 0; j < i; j++) {
   //     [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
   //   }
   // }
---
+++ Other/Fuzzy Find.js
@@ -0,0 +1,4 @@
+//given a string, 's', find if the letters in s show up
+//in a number of strings(in an array) in the order they appear
+//return the strings that they show up in, in an array
+
