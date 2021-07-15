a94fc43004cacf106d70e2ab0dbe77098544d512
David <david.pok@gmail.com>
2021-07-14 17:09:49 -0700

    adds solution

--- LCode/other/67 Add Binary.js
+++ LCode/other/67 Add Binary.js
@@ -1,7 +1,5 @@
 // Given two binary strings a and b, return their sum as a binary string.
 
- 
-
 // Example 1:
 
 // Input: a = "11", b = "1"
@@ -10,7 +8,6 @@
 
 // Input: a = "1010", b = "1011"
 // Output: "10101"
- 
 
 // Constraints:
 
@@ -18,3 +15,29 @@
 // a and b consist only of '0' or '1' characters.
 // Each string does not contain leading zeros except for the zero itself.
 
+let addBinary = (a, b) => {
+  // Truth Table
+  // 1st + 2nd + carry = sum, new carry, decimal sum
+  //   0 +  0  + 0 = 0, 0 (0)
+  //   0 +  0  + 1 = 1, 0 (1)
+  //   0 +  1  + 1 = 0, 1 (2)
+  //   1 +  1  + 1 = 1, 1 (3)
+
+  let carry = 0;
+  let result = "";
+
+  let len1 = a.length - 1;
+  let len2 = b.length - 1;
+
+  for (; len1 >= 0 || len2 >= 0 || carry > 0; len1--, len2--) {
+    let sum = (+a[len1] || 0) + (+b[len2] || 0) + carry;
+    if (sum > 1) {
+      sum = sum % 2;
+      carry = 1;
+    } else {
+      carry = 0;
+    }
+    result = `${sum}${result}`;
+  }
+  return result;
+};
