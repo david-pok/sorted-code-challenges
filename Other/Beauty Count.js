3fda27e8cfe816c616b49d3c278e2631d6b647cf
David <david.pok@gmail.com>
2021-07-19 16:58:03 -0700

    adds pseudo code

--- Other/Beauty Count.js
+++ Other/Beauty Count.js
@@ -1,30 +1,30 @@
-// When John was a little kid he didn't have much to do. 
-// There was no internet, no Facebook, and no programs 
-// to hack on. So he did the only thing he could… he 
-// evaluated the beauty of strings in a quest to discover 
+// When John was a little kid he didn't have much to do.
+// There was no internet, no Facebook, and no programs
+// to hack on. So he did the only thing he could… he
+// evaluated the beauty of strings in a quest to discover
 // the most beautiful string in the world.
-// Given a string s, little Johnny defined the beauty 
+// Given a string s, little Johnny defined the beauty
 // of the string as the sum of the beauty of the letters in it.
-// The beauty of each letter is an integer between 
-// 1 and 26, inclusive, and no two letters have the 
-// same beauty. Johnny doesn't care about whether 
-// letters are uppercase or lowercase, so that doesn't 
-// affect the beauty of a letter. (Uppercase 'F' is 
+// The beauty of each letter is an integer between
+// 1 and 26, inclusive, and no two letters have the
+// same beauty. Johnny doesn't care about whether
+// letters are uppercase or lowercase, so that doesn't
+// affect the beauty of a letter. (Uppercase 'F' is
 // exactly as beautiful as lowercase 'f', for example.)
-// You're a student writing a report on the youth of this 
-// famous hacker. You found the string that Johnny 
-// considered most beautiful. What is the maximum 
+// You're a student writing a report on the youth of this
+// famous hacker. You found the string that Johnny
+// considered most beautiful. What is the maximum
 // possible beauty of this string?
 
 // Input
-// The input file consists of a single integer m followed 
+// The input file consists of a single integer m followed
 // by m lines.
 
 // Output
-// Your output should consist of, for each test case, a 
-// line containing the string "Case #x: y" where x is the 
-// case number (with 1 being the first case in the 
-//     input file, 2 being the second, etc.) and y 
+// Your output should consist of, for each test case, a
+// line containing the string "Case #x: y" where x is the
+// case number (with 1 being the first case in the
+//     input file, 2 being the second, etc.) and y
 //     is the maximum beauty for that test case.
 
 // Constraints
@@ -46,3 +46,20 @@
 // Case #4: 729
 // Case #5: 646
 
+const BeautyCount = (input) => {
+    //check input and break down into separate strings if not already
+    //run through each individual strings
+    //check if ascii character is a letter
+    //if it is a letter keep a count on how many times each char shows up
+    //once done sort in descending with largest number getting their count
+    //multiplied by 26, 2nd by 25, 3rd by 24, etc
+    //tally up totals and return per string
+};
+
+const inpt = [
+  "ABbCcc",
+  "Good luck in the Facebook Hacker Cup this year!",
+  "Ignore punctuation, please 🙂",
+  "Sometimes test cases are hard to make up.",
+  "So I just go consult Professor Dalves",
+];
